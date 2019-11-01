8862fe47c9d73ad2da1cd5d5c332b669701b82ff
sabanansari <alisaban52@gmail.com>
2019-11-02 02:21:24 +0530

    started project basic UI

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        children: <Widget>[
+          Icon(Icons.list),
+          Text('Todoey'),
+        ],
+      ),
+    );
+  }
+}
